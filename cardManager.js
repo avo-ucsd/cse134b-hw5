8c2dab90078f21e745f50b8d3049461ae8e8457a
asvo <asvo@ucsd.edu>
2025-12-02 19:17:28 -0800

    Add Load Local functionality (localStorage implementation)

--- cardManager.js
+++ cardManager.js
@@ -3,23 +3,10 @@ const key = 'projects';
 window.addEventListener('DOMContentLoaded', async () => { 
     fillStorage();
 
-    const card = document.createElement('project-card');
+    const loadLocalBtn = document.querySelector('#hw5-data-loading > button');
+    const loadRemoteBtn = document.querySelector('#hw5-data-loading > button:nth-child(2)');
 
-    card.data = {
-        imageSrc: './images/project/project_letMeCook-600.png',
-        imageAlt: 'Let Me Cook app icon',
-        imageSizes: [
-            { url: './images/project/project_letMeCook-350.png', width: 350 },
-            { url: './images/project/project_letMeCook-600.png', width: 600 },
-            { url: './images/project/project_letMeCook-900.png', width: 900 }
-        ],
-        title: 'Let Me Cook',
-        description: 'A recipe web app to create, store, and view personal recipes. I helped lead our quarter long project for CSE 110: Software Engineering at the University of California, San Diego.',
-        link: 'https://github.com/cse110-sp25-group8/final-project',
-        linkText: 'View more on GitHub'
-    };
-
-    document.querySelector('.project-list').appendChild(card);
+    loadLocalBtn.addEventListener('click', loadLocal);
 });
 
 function fillStorage() {
@@ -51,10 +38,18 @@ function fillStorage() {
         linkText: 'Sorry, the bot owner has the repository private.'
     };
 
-    const myProjects = [
-        JSON.stringify(projectData1),
-        JSON.stringify(projectData2)
-    ];
+    const myProjects = [projectData1, projectData2];
 
-    localStorage.setItem(key, myProjects);
+    localStorage.setItem(key, JSON.stringify(myProjects));
+}
+
+function loadLocal() {
+    const storageData = localStorage.getItem(key);
+    const projects = JSON.parse(storageData);
+
+    for (const projectData of projects) {
+        const card = document.createElement('project-card');
+        card.data = projectData;
+        document.querySelector('.project-list').appendChild(card);
+    }
 }
